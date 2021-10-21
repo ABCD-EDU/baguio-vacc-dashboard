013509accac073066e66faf50054ca585279d926
Arian Cayton <ariancayton@gmail.com>
2021-10-21 20:22:49 +0800

    fix: change JSON keys into camelCase

--- res/data/generate.py
+++ res/data/generate.py
@@ -15,9 +15,10 @@ category_list = ["A1","A2","A3","A4","A5","B&C"]
 
 curr_id_number = 1
 brgy_data["id"] = curr_id_number
-
-with open('data.csv', 'r') as file:
-    # lines = file.readlines()
+import os
+print('curr working directory')
+print(os.getcwd())
+with open('./res/data/data.csv', 'r') as file:
 
     lines = csv.reader(file, delimiter= ',', quotechar='"')
     for index, line in enumerate(lines):
@@ -32,12 +33,12 @@ with open('data.csv', 'r') as file:
         vaccine_type_data = {}
         for index_inner, vaccine_type in enumerate(vaccine_type_list):
             vaccine_type_data[vaccine_type] = values[index_inner+4]
-        brgy_data["vaccine-type"] = vaccine_type_data
+        brgy_data["vaccineType"] = vaccine_type_data
 
         age_group_data = {}
         for index_inner, age_group in enumerate(age_group_list):
             age_group_data[age_group] = values[index_inner+13]
-        brgy_data["age-group"] = age_group_data
+        brgy_data["ageGroup"] = age_group_data
 
         category_data = {}
         for index_inner, category in enumerate(category_list):
@@ -47,9 +48,10 @@ with open('data.csv', 'r') as file:
         per_sex_data = {}
         per_sex_data["M"] = values[index_inner+34]
         per_sex_data["F"] = values[index_inner+35]
-        brgy_data["per_sex"] = per_sex_data
+        brgy_data["perSex"] = per_sex_data
 
         data.append(brgy_data.copy())
         curr_id_number+=1
-with open('output.json','w') as outfile:
+        
+with open('./res/data/output.json','w') as outfile:
     json.dump(data, outfile,indent=4)
